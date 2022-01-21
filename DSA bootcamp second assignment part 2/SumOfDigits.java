f99d5241554a7f5aeadab8386c88a4562c48f2c2
Abbas Khan <78753275+Abbas-glitch@users.noreply.github.com>
2022-01-21 13:31:45 +0530

    Intermediate Conditionals and Loops problems

--- DSA bootcamp second assignment part 2/SumOfDigits.java
+++ DSA bootcamp second assignment part 2/SumOfDigits.java
@@ -2,18 +2,17 @@
 
 import java.util.Scanner;
 
-public class SumOfDigits {
+public class SumOfDigits {   // 1234 = 1+2+3+4 = 10
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number : ");
+        System.out.println("Enter a number : ");
         int n = sc.nextInt();
         int sum = 0;
-        while (n>0) {
+        while (n > 0){
             int rem = n % 10;
-            sum+=rem;
+            sum += rem;
             n/=10;
         }
-        System.out.println("Sum of digits of this number is : "+sum);
-
+        System.out.println(sum);
     }
 }
